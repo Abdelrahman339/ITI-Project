139c5fd3c96f4fa121c4cd9b9af30914a0f86131
Abdelrahman339 <abdelrahmanamr.eltawab@gmail.com>
2023-09-29 22:50:04 +0300

    deleted react

---
+++ React_project/iti-react/src/components/Abdelrahman_components/About_us_button.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+function About_us_button() {
+    return (
+        <div className="button">
+            <button>clik here</button>
+        </div>
+    );
+}
+export default About_us_button;
