05f892ad2a310538cdc55951141c6c3304a8df30
yassmen1 <yassmen1030@gmail.com>
2023-09-29 23:14:39 +0300

    edit inn branch

--- React_project/iti-react/src/App.js
+++ React_project/iti-react/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-
+// import './App.css';
+import React from 'react';
+import Navbar from './components/Yassmen_components/header.js';
 
 function App() {
   return (
     <div className="App">
-
+        <Navbar/>
     </div>
   );
 }
---
+++ React_project/iti-react/src/components/Yassmen_components/header.js
@@ -0,0 +1,18 @@
+import React from "react"
+function Navbar() {
+    return (
+        <div class="Navbar">
+            <ul>
+                <li><a href="#Home">Home</a></li>
+                <li><a href="#Menu">Menu</a></li>
+                <li><a href="#Pages">Pages</a></li>
+                <li><a href="#About">About US</a></li>
+                <li><a href="#Special-Recipes">Special-Recipes</a></li>
+                <li><a href="#Delivery">Delivery</a></li>
+                <li><a href="#Form">Form</a></li>
+                <li><a class="Table-Booking" href="#Table-Booking">Table-Booking</a></li>
+            </ul>
+        </div>
+    )
+}
+export default Navbar
