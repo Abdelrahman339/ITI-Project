e42b19eca4ff81b133adea992d417bddf76487d2
yassmen1 <yassmen1030@gmail.com>
2023-09-30 02:16:17 +0300

    added header and navbar and homepage react

--- React_project/iti_project/src/App.js
+++ React_project/iti_project/src/App.js
@@ -6,41 +6,62 @@ import Photos2 from "./components/Abdelrahman_components/Photos2";
 import Special_recipes from './components/Abdelrahman_components/Special_recipes';
 import Photos3 from './components/Abdelrahman_components/Photos3';
 import Photos4 from './Photos4';
+import Before from "./components/yassmen component/before";
+import Navbar from "./components/yassmen component/navbar";
+import HomePage from "./components/yassmen component/homePage";
 import './App.css'
 function App() {
   return (
-    <div className="App">
-      <div className="section1">
-        <div className="container">
-          <div className="Photos1">
-            <Photos1 />
-          </div>
-          <div className="about-us">
-            <About_us />
+    <div>
+      <header>
+        <div className="before">
+          <Before />
+        </div>
+        <div className="navbar" id="Home">
+        <Navbar/>
+        </div>
+      </header>
+      <section>
+                <div class="content3" id="Home">
+                    <div class="cont">
+                      <HomePage/>
+                    </div>
+                </div>
+            </section>
 
-            <div className="button">
-              <Button />
+      <div className="App">
+        <div className="section1">
+          <div className="container">
+            <div className="Photos1">
+              <Photos1 />
             </div>
-          </div>
-          <div className="Photos2">
-            <Photos2 />
+            <div className="about-us">
+              <About_us />
+
+              <div className="button">
+                <Button />
+              </div>
+            </div>
+            <div className="Photos2">
+              <Photos2 />
+            </div>
           </div>
         </div>
-      </div>
-      {/* #################################### */}
-      <div className="section2">
-        <div className="contanier">
-          <div className="special-recipes">
-            <Special_recipes />
-            <div className="button">
-              <Button />
+
+        <div className="section2">
+          <div className="contanier">
+            <div className="special-recipes">
+              <Special_recipes />
+              <div className="button">
+                <Button />
+              </div>
             </div>
-          </div>
-          <div className="photos3">
-          <Photos3/>
-          </div>
-          <div className="photos4">
-          <Photos4 />
+            <div className="photos3">
+              <Photos3 />
+            </div>
+            <div className="photos4">
+              <Photos4 />
+            </div>
           </div>
         </div>
       </div>
---
+++ React_project/iti_project/src/components/yassmen component/homePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function HomePage() {
+
+    return (
+        <section>
+            <div class="content3" id="Home">
+                <div class="cont">
+                    <h1> Foody Love</h1>
+                    <p>Sed ut perspiciatis unde omnis iste natus
+                        error sit voluptatem accusantium doloremque laudantium,
+                        totam rem aperiam,
+                        eaque ipsa quae ab illo inventore veritatis et quasi.
+                    </p>
+                </div>
+                <div class="content-btn">
+                    <a href="#main-btn">Click Here</a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default HomePage;
