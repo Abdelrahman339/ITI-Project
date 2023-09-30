467d3bb5db25019df5cfc97dac525f3f1dcb4537
Abdelrahman339 <abdelrahmanamr.eltawab@gmail.com>
2023-09-30 16:46:15 +0300

    added philo part and change in react

---
+++ React_project/iti_project/src/components/Abdelrahman_components/Photos4.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Photos4() {
+    return (
+        <div className="Photos4">
+            <img src={require('./images/special-recipes/salad photo.png')} />
+        </div>
+    );
+}
+export default Photos4;
