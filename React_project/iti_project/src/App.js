62e1681afdb36dd608bf451d8a349d62fc1ca169
nouran mahmoud <nouranmahmoud243@gmail.com>
2023-09-30 05:09:22 -0700

    style change

--- React_project/iti_project/src/App.js
+++ React_project/iti_project/src/App.js
@@ -1,6 +1,6 @@
 
 import About_us from "./components/Abdelrahman_components/About_us";
-// import Button from "./components/Abdelrahman_components/button";
+import Button from "./components/Abdelrahman_components/button";
 // import Photos1 from "./components/Abdelrahman_components/Photos1";
 import Photos2 from "./components/Abdelrahman_components/Photos2";
 import Special_recipes from './components/Abdelrahman_components/Special_recipes';
@@ -10,8 +10,7 @@ import Before from "./components/yassmen component/before";
 import Navbar from "./components/yassmen component/navbar";
 import HomePage from "./components/yassmen component/homePage";
 import './App.css'
-import Paragraph from "./components/nouran component/paragraph";
-import Grid from "./components/nouran component/component";
+import paragraph from "./components/nouran component/paragraph";
 function App() {
   return (
     <>
@@ -24,29 +23,23 @@ function App() {
           <div className="contanier">
             <div className="special-recipes">
               <Special_recipes />
+              <Button />
             </div>
             <Photos3 />
             <Photos4 />
           </div>
         </div>
       </div>
-      <div class="parent" id="Pages">
-
-        <Paragraph />
-
-        <div class="grid-container">
-          <Grid/>
-
-        </div>
-        </div>
-      </>
-      );
+      <Paragraph/>
+      <Container/>
+    </>
+  );
 }
 
-      // ############################################################################
-      // ############################################################################
-      // ############################################################################
-      export default App;
+// ############################################################################
+// ############################################################################
+// ############################################################################
+export default App;
 
 
 
