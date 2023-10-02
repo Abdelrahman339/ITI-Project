0975404003b78edfca5bf7af83c719ec610ecd2b
Abdelrahman339 <abdelrahmanamr.eltawab@gmail.com>
2023-10-02 14:23:21 +0300

    added fleopater components

---
+++ React_project/iti_project/src/components/fleopater component/function2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function feedback() {
+    return (
+        <div class="feedback">
+            <h2>Feedback</h2>
+            <form action="" />
+            <input type="text" placeholder="Name" /><br />
+            <input type="text" placeholder="Email" /><br />
+            <input type="text" placeholder="Phone" /><br />
+            <input type="text" placeholder="Message" /><br />
+            <button onclick="showAlert()">submit</button>
+        </div>
+    )
+}
