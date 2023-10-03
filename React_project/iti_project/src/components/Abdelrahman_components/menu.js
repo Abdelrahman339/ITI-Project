763d7d3022433e1219618657229a6ddc9bc0de95
Abdelrahman339 <abdelrahmanamr.eltawab@gmail.com>
2023-10-03 19:36:14 +0300

    edit in menu

--- React_project/iti_project/src/components/Abdelrahman_components/menu.js
+++ React_project/iti_project/src/components/Abdelrahman_components/menu.js
@@ -4,26 +4,29 @@ import Photos2 from './Photos2';
 function Menu() {
     return (
         <div className='Menu' id='Menu'>
-                <div className='Menu-heading'> 
+            <div className='Menu-heading'>
                 <span>Amazing</span>
                 <p>delicious</p>
-                </div>
+            </div>
             <div className="menu-contanier">
                 <div className="image">
                     <img src={require("../images/amazing-delicious/amazing delicious 1.png")} alt="Drinks" />
                 </div>
-                    <div className="menu-card">
+                <div className="menu-card">
+                    <div className='menu-para'>
                         <h1>Drinks</h1>
                         <p> <i class="fa-solid fa-wine-glass"></i> Menu item 1........................$10.99</p>
                         <p> <i class="fa-solid fa-wine-glass"></i> Menu item 2........................$12.99</p>
                         <p> <i class="fa-solid fa-wine-glass"></i> Menu item 3........................$9.99</p>
                         <p> <i class="fa-solid fa-wine-glass"></i> Menu item 4........................$11.99</p>
                         <p> <i class="fa-solid fa-wine-glass"></i> Menu item 5........................$13.99</p>
+                    </div>
                 </div>
             </div>
             <br />
             <div className="menu-contanier">
-                    <div className="menu-card">
+                <div className="menu-card-middle">
+                    <div className='menu-para'>
                         <h1>Main Course</h1>
                         <p> <i class="fa-solid fa-pizza-slice"></i> Menu item 1.....................$10.99</p>
                         <p> <i class="fa-solid fa-pizza-slice"></i> Menu item 2.....................$12.99</p>
@@ -31,22 +34,25 @@ function Menu() {
                         <p> <i class="fa-solid fa-pizza-slice"></i> Menu item 4.....................$11.99</p>
                         <p> <i class="fa-solid fa-pizza-slice"></i> Menu item 5.....................$13.99</p>
                     </div>
+                </div>
                 <div className="image2">
                     <img src={require("../images/amazing-delicious/amazing delicious 2.png")} alt="Main Course" />
                 </div>
             </div>
             <br />
             <div className="menu-contanier">
-                    <div className="menu-card">
+                <div className="image3">
+                    <img src={require("../images/amazing-delicious/amazing delicious 3.png")} alt="Desserts" />
+                </div>
+                <div className="menu-card">
+                    <div className='menu-para'>
                         <h1>Desserts</h1>
                         <p> <i class="fa-solid fa-cookie-bite"></i> Menu item 1........................$10.99</p>
                         <p> <i class="fa-solid fa-cookie-bite"></i> Menu item 2........................$12.99</p>
                         <p> <i class="fa-solid fa-cookie-bite"></i> Menu item 3........................$9.99</p>
                         <p> <i class="fa-solid fa-cookie-bite"></i> Menu item 4........................$11.99</p>
                         <p> <i class="fa-solid fa-cookie-bite"></i> Menu item 5........................$13.99</p>
-                </div>
-                <div className="image3">
-                    <img src={require("../images/amazing-delicious/amazing delicious 3.png")} alt="Desserts" />
+                    </div>
                 </div>
             </div>
             <Photos2 />
