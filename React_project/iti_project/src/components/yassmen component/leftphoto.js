bb18ef4b2172cc143ba2c17f15d8929c79db7fd3
Abdelrahman339 <abdelrahmanamr.eltawab@gmail.com>
2023-10-03 20:26:47 +0300

    added left and right photos

---
+++ React_project/iti_project/src/components/yassmen component/leftphoto.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Leftphoto() {
+    return (
+        <div className="Photos1">
+            <img src={require('../images/footer/left photo footer.png')} alt='' />
+        </div>
+    );
+}
+export default Leftphoto;
---
+++ React_project/iti_project/src/components/yassmen component/rigtphoto.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Rightphoto() {
+    return (
+        <div className="Photos1">
+            <img src={require('../images/delightful-experience/spaghatii.png')} alt='' />
+        </div>
+    );
+}
+export default Rightphoto;
