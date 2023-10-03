e671b92ff2a898752e46b625303fdac7e1ad1530
Abdelrahman339 <abdelrahmanamr.eltawab@gmail.com>
2023-10-03 20:35:23 +0300

    edit in left photo

--- React_project/iti_project/src/components/yassmen component/footer.js
+++ React_project/iti_project/src/components/yassmen component/footer.js
@@ -3,15 +3,18 @@ import FeedBack from './feedback';
 import WorkingHours from './working hours';
 import Location from './location';
 import CoptyRight from './copyRight';
+import Leftphoto from './leftphoto';
+import Rightphoto from './rigtphoto';
 
 function Footer() {
 
     return (
         <div className='Footer'>
-
+            <Leftphoto/>
             <Location />
             <WorkingHours />
             <FeedBack />
+            <Rightphoto/>
             <CoptyRight />
         </div>
     );
